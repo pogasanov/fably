7639259741eb2cf004098bf7d40fa4416cb31398
Pavel <pogasanov@gmail.com>
2023-06-22 10:23:57 +0600

    Fix build warnings

--- src/components/shared/ui/Header/index.tsx
+++ src/components/shared/ui/Header/index.tsx
@@ -24,8 +24,6 @@ const Header = () => {
                         className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
                         aria-expanded="false" data-dropdown-toggle="dropdown-user">
                   <span className="sr-only">Open user menu</span>
-                  <img className="w-8 h-8 rounded-full"
-                       src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo"/>
                 </button>
               </div>
               <div
